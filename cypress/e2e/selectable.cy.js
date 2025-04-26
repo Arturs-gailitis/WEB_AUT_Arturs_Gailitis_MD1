5599d8c1f08e344843e3eb2a7c2f48032023854e
Arturs-gailitis <artursgailitis2003@gmail.com>
2025-04-26 13:05:00 +0300

    commit_4

--- cypress/e2e/selectable.cy.js
+++ cypress/e2e/selectable.cy.js
@@ -5,20 +5,30 @@ describe('demoqa', () => {
         
         it('testing Grid Page', () => {
 
+            //Open https://demoqa.com/selectable
             GridPage.visit();
 
+            //Click “Grid”
             GridPage.clickGrid.click();
             
+            //Click- “Two”, “Four”, “Six”, “Eight”
             GridPage.clickNumbers.contains('Two').click();
             GridPage.clickNumbers.contains('Four').click();
             GridPage.clickNumbers.contains('Six').click();
             GridPage.clickNumbers.contains('Eight').click();
 
+            //Validate that “Two”, “Four”, “Six”, “Eight” are highlighted
             GridPage.verifyHighlighted.contains('Two');
             GridPage.verifyHighlighted.contains('Four');
             GridPage.verifyHighlighted.contains('Six');
             GridPage.verifyHighlighted.contains('Eight');
 
+            //Validate that “One”, “Three”, “Five”, “Seven”, “Nine” are not highlighted
+            GridPage.clickNumbers.contains('One').should('not.have.class', 'active');
+            GridPage.clickNumbers.contains('Three').should('not.have.class', 'active');
+            GridPage.clickNumbers.contains('Five').should('not.have.class', 'active');
+            GridPage.clickNumbers.contains('Seven').should('not.have.class', 'active');
+            GridPage.clickNumbers.contains('Nine').should('not.have.class', 'active');
         })
     })
 })
