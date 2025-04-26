4064eea740eeb79283f05c12b4264298010992cf
Arturs-gailitis <artursgailitis2003@gmail.com>
2025-04-26 12:48:09 +0300

    commit_2

--- cypress/e2e/selectable.cy.js
+++ cypress/e2e/selectable.cy.js
@@ -4,7 +4,16 @@ describe('demoqa', () => {
     context('Grid Page', () => {
         
         it('testing Grid Page', () => {
+
             GridPage.visit();
+
+            GridPage.clickGrid.click();
+            
+            GridPage.clickNumbers.contains('Two').click();
+            GridPage.clickNumbers.contains('Four').click();
+            GridPage.clickNumbers.contains('Six').click();
+            GridPage.clickNumbers.contains('Eight').click();
+
         })
     })
 })
--- cypress/pageObjects/GridPage.js
+++ cypress/pageObjects/GridPage.js
@@ -4,4 +4,12 @@ export class GridPage extends BasePage {
     static get url() {
         return 'selectable';
     }
+
+    static get clickGrid() {
+        return cy.get('a#demo-tab-grid');
+    }
+
+    static get clickNumbers() {
+        return cy.get('li.list-group-item');
+    }
 }
